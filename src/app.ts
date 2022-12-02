b2c093d33b059034c519284677001180d77278fd
Dipoengoro <dipoengoro@outlook.com>
2022-12-03 00:04:22 +0700

    Step 2 - setup views folder

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,33 @@
 import * as express from "express";
 import * as process from "process";
+import * as path from "path";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-   res.send("<h1>Hello World!</h1>");
+app.get("/about", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../views", "about.html");
+    res.sendFile(htmlFilePath);
 });
 
+app.get("/confirm", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../views", "confirm.html");
+    res.sendFile(htmlFilePath);
+});
+
+app.get("/", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../views", "index.html");
+    res.sendFile(htmlFilePath);
+});
+
+app.get("/recommend", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../views", "recommend.html");
+    res.sendFile(htmlFilePath);
+});
+
+app.get("/restaurants", (req, res) => {
+    const htmlFilePath = path.join(__dirname, "../views", "restaurants.html");
+    res.sendFile(htmlFilePath);
+});
 app.listen(PORT, () => {
     console.log(`Server is running in http://localhost:${PORT}`);
 });
