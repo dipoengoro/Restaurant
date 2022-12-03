d3c33843b940cb600ca6246f70eaba863d70a674
Dipoengoro <dipoengoro@outlook.com>
2022-12-03 12:39:27 +0700

    Good

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static("../public"));
 app.use(express.urlencoded({extended: false}));
 
 const filePath = path.join(__dirname, "..", "data", "restaurants.json");
