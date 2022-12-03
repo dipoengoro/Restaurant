a79955b901fe464ca922c97c317e6702a621cbc5
dipoengoro <dipoengoro@outlook.com>
2022-12-03 07:12:26 +0000

    Deploying to gh-pages from @ dipoengoro/Restaurant@b9a164ca644a7e59f2debe60dc65963b1f028585 🚀

--- public/scripts/responsive.js
+++
@@ -1,6 +0,0 @@
-var drawerBtnElement = document.getElementById("drawer-btn");
-var mobileDrawerElement = document.getElementById("mobile-drawer");
-function toggleDrawer() {
-    mobileDrawerElement.classList.toggle("open");
-}
-drawerBtnElement.addEventListener("click", toggleDrawer);
